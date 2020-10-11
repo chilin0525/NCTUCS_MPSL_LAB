2bee0c15e129cb086ac735f25c659aa9595691c9
Chliin <sky707070.cv07@nctu.edu.tw>
2020-10-11 12:03:21 +0800

    Update test.py

--- LAB2/Code/test.py
+++ LAB2/Code/test.py
@@ -1,7 +1,7 @@
-a1 = 30000
-a2 = 40000
-b1 = 21456
-b2 = 65487
+a1 = 0x1234
+a2 = 0x5678
+b1 = 0x7765
+b2 = 0x4321
 
 z1 = a1*b1
 z2 = a2*b2
@@ -9,4 +9,16 @@ z3 = (a1+a2)*(b1+b2)-z1-z2
 
 ans = z1*pow(10,10)+z3*pow(10,5)+z2
 
-print(ans)
+print(0x12345678*0x77654321)
+print(hex(0x12345678*0x77654321))
+print(hex(a1*b1))
+print(hex(a2*b2))
+print(hex(z3))
+
+print("\n")
+
+tmp1 = 0x16ac8d78
+tmp2 = 0xda0c0000
+
+t1 = tmp1 + tmp2
+print(hex(t1))
