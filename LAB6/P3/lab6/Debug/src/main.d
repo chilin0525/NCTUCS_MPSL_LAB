1b753dde3a1140828dd602742bc692d262af9aae
Chliin <sky707070.cv07@nctu.edu.tw>
2020-11-24 20:40:09 +0800

    Add files via upload

---
+++ LAB6/P3/lab6/Debug/src/main.d
@@ -0,0 +1,17 @@
+src/main.o: ../src/main.c ../src/stm32l476xx.h ../src/core_cm4.h \
+ ../src/core_cmInstr.h ../src/cmsis_gcc.h ../src/core_cmFunc.h \
+ ../src/core_cmSimd.h ../src/system_stm32l4xx.h
+
+../src/stm32l476xx.h:
+
+../src/core_cm4.h:
+
+../src/core_cmInstr.h:
+
+../src/cmsis_gcc.h:
+
+../src/core_cmFunc.h:
+
+../src/core_cmSimd.h:
+
+../src/system_stm32l4xx.h:
