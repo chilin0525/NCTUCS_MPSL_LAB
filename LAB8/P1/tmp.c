ade1421441f23b76cc351c528b12e154a4185b7c
Chliin <sky707070.cv07@nctu.edu.tw>
2020-12-09 02:01:24 +0800

    Update tmp.c

--- LAB8/P1/tmp.c
+++ LAB8/P1/tmp.c
@@ -8,27 +8,31 @@ extern void MAX7219Send(int address,int data);
 extern void MUTIDISPLAY();
 
 void UART2_Transmit(char* data) {
-	while(*data) UART2_Tra(*data++);
+	while(*data){
+		UART2_Tra(*data++);
+	} 
 }
 
 void UART2_Tra(char data){
-	while(!(USART2->ISR) & USART_ISR_TXE);
+	while(!(USART2->ISR & USART_ISR_TXE));
 	USART2->TDR = data;
 }
 
 void UART2_init() {
-	RCC->APB2ENR |= RCC_APB2ENR_USART1EN;
+	RCC->APB1ENR1 |= RCC_APB1ENR1_USART2EN;
 
 	// CR1
-	USART2->CR1 = (USART_CR1_TE | USART_CR1_RE);
+	USART2->CR1 &= ~(USART_CR1_M | USART_CR1_PS | USART_CR1_PCE | USART_CR1_TE | USART_CR1_OVER8);
+	USART2->CR1 |= (USART_CR1_TE | USART_CR1_RE) ;
 
 	// baud rate
 	// 4M = 4000000
-	USART2->BRR = 4000000;
+	USART2->BRR = 4000000/9600;
 
 	// CR2
 	// 1 stop bit
 	USART2->CR2 = 0;
+
 
 	// Uart Enable 
 	USART2->CR1 |= USART_CR1_UE;
@@ -38,11 +42,13 @@ void GPIO_init(){
 	RCC->AHB2ENR |= 0x7;
 
 	GPIOA->MODER = 0b11110000; 
+	// change to AF mode
+
 	GPIOC->MODER = 01;
 	// pa2 , pa3
 	// pc13 as btn
-
-	GPIOA->AFR[0] |= 0x10000000;
+	
+	GPIOA->AFR[0] |= 0x00007700;
 }
 
 int DEBOUNCE(){
@@ -84,14 +90,14 @@ int main(){
 	UART2_init();
 
 	char* Str = "Hello world\n";
-		GPIOC->ODR = 0;
+	GPIOC->ODR = 0;
 
 	while(1){
 		// check pc13
 		//if((GPIOC->IDR & 0b10000000000000) == 0){
 		if(DEBOUNCE()==1){
 			GPIOC->ODR = GPIOC->ODR ^ 1;
-			//UART2_Transmit(Str);
+			UART2_Transmit(Str);
 		}
 	}
 
