2084334a693608b40afc4878ee375496b64eac61
Chliin <sky707070.cv07@nctu.edu.tw>
2020-12-09 00:49:09 +0800

    Create debounce.c

---
+++ codebook/debounce.c
@@ -0,0 +1,33 @@
+int DEBOUNCE(){
+    int i = 1;
+    int press_cnt = 0;
+    int press_flag = 0;
+    int press_loose_flag = 0;
+
+    for(i=1;i<=1000;i++){
+        int check_press = GPIOC -> IDR & 0b10000000000000;
+        if(check_press != 0){   // not press?
+            press_cnt = 0;
+        } else {                // press?
+            ++press_cnt;
+        }
+
+        if(press_cnt >= 300){
+            press_flag = 1;
+            break;
+        }
+    }
+
+    if(press_flag){
+        while(1){
+            int check_press = GPIOC -> IDR & 0b10000000000000;
+            if(check_press != 0){
+                break;
+            }
+        }
+    } else {
+        return 0;   // not press
+    }
+
+    return 1;   // press
+}
